e54102b19993f5d4c7fdb1ead2561570d0ccb760
LinYuMingTaipei <a828215362@gmail.com>
2021-01-03 18:10:01 +0800

    fix redis host

--- Urmart/settings.py
+++ Urmart/settings.py
@@ -129,8 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Celery
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
