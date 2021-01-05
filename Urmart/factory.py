ec9bab89e559db8de0385004b911b8407233e9b9
LinYuMingTaipei <a828215362@gmail.com>
2021-01-05 12:44:15 +0800

    modify celery config

--- Urmart/factory.py
+++ Urmart/factory.py
@@ -8,8 +8,8 @@ from Urmart import settings
 
 def create_app():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Urmart.settings')
-    app = Celery(__name__)
     get_wsgi_application()
+    app = Celery(__name__)
     app.config_from_object('django.conf:settings')
     app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
     return app
